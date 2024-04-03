cd0d26e02f6b4990488fdb10ee9e3cfad6cae679
I Guest <i.guest2010@gmail.com>
2024-04-03 15:37:39 +0100

    Build out ASP.NET backend

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,10 @@
+using DbUp;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace QandA
 {
@@ -26,6 +20,23 @@ namespace QandA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            EnsureDatabase.For.SqlDatabase(connectionString);
+
+            var upgrader = DeployChanges.To
+                .SqlDatabase(connectionString, null)
+                .WithScriptsEmbeddedInAssembly(
+                System.Reflection.Assembly.GetExecutingAssembly()
+                )
+                .WithTransaction()
+                .LogToConsole()
+                .Build();
+
+            if (upgrader.IsUpgradeRequired())
+            {
+                var result = upgrader.PerformUpgrade();
+            }
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
