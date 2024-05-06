3ddffbe141251f1013c948b9589162d042250dad
I Guest <i.guest2010@gmail.com>
2024-05-06 18:28:10 +0100

    implement api controller

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using QandA.Data;
 
 namespace QandA
 {
@@ -43,6 +44,8 @@ namespace QandA
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "QandA", Version = "v1" });
             });
+
+            services.AddScoped<IDataRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
